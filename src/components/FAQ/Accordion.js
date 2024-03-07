b1f50679c3a47262ac9ff454a13ae70cc3f109b8
Aditya_Mishra <adityasaiyansuper@gmail.com>
2024-03-07 20:48:49 +0530

    responsive faq

--- src/components/FAQ/Accordion.js
+++ src/components/FAQ/Accordion.js
@@ -11,8 +11,8 @@ const Accordion = ({ title, content }) => {
           className="accordion-title "
           onClick={() => setIsActive(!isActive)}
         >
-          <div className="col-lg-10">{title}</div>
-          <div className="col-lg-2 sign">{isActive ? "-" : "+"}</div>
+          <div className="col-lg-10 col-md-10 col-sm-10 col-10">{title}</div>
+          <div className="col-lg-2 col-md-2 col-sm-2 col-2 sign">{isActive ? "-" : "+"}</div>
         </div>
         {isActive && <div className="accordion-content">{content}</div>}
       </div>
